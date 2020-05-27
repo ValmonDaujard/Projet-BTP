4940906e6a79d3a41c123c50e05adbe504b99270
Charline <charline>
2020-05-27 16:27:39 +0200

    Auto stash before merge of "master" and "origin/master"

--- BTP/src/app/app.component.ts
+++ BTP/src/app/app.component.ts
@@ -11,19 +11,19 @@ export class AppComponent {
   constructor(public router: Router){}
 
   w3_open() {
-    document.getElementById("main").style.marginLeft = "20%";
-    document.getElementById("mySidebar").style.width = "20%";
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("openNav").style.display = 'none';
-    document.getElementById("titre").style.marginLeft = "5%";
-    document.getElementById("myOverlay").style.display = "block";
+    document.getElementById('main').style.marginLeft = '20%';
+    document.getElementById('mySidebar').style.width = '20%';
+    document.getElementById('mySidebar').style.display = 'block';
+    document.getElementById('openNav').style.display = 'none';
+    document.getElementById('titre').style.marginLeft = '5%';
+    document.getElementById('myOverlay').style.display = 'block';
   }
   w3_close() {
-    document.getElementById("main").style.marginLeft = "0%";
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("openNav").style.display = "inline-block";
-    document.getElementById("titre").style.marginLeft = "0%";
-    document.getElementById("myOverlay").style.display = "none";
+    document.getElementById('main').style.marginLeft = '0%';
+    document.getElementById('mySidebar').style.display = 'none';
+    document.getElementById('openNav').style.display = 'inline-block';
+    document.getElementById('titre').style.marginLeft = '0%';
+    document.getElementById('myOverlay').style.display = 'none';
   }
 }
 
