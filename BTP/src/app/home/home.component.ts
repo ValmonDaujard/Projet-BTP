b415aac5c4c6dc320542ac1c19fb08dfffdb5998
Valmon Daujard <valmon.daujard@gmail.com>
2020-05-29 14:34:01 +0200

    Auto stash before merge of "master" and "origin/master"

--- BTP/src/app/home/home.component.ts
+++ BTP/src/app/home/home.component.ts
@@ -11,6 +11,7 @@ import {Offre} from '../model/offre';
 })
 export class HomeComponent implements OnInit {
 
+  search : string;
   projets: Array<Projet> = new Array<Projet>();
   offres: Array<Offre> = new Array<Offre>();
 
@@ -21,9 +22,46 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
   list(id: number){
     this.homeService.findAllByMaitreOeuvre(id).subscribe(resp => this.projets = resp, error => console.log(error));
     this.homeService.findAllByMaitreOeuvreEnConsult(id).subscribe(resp => this.offres = resp, error => console.log(error));
   }
 
+  rechercheProj() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("projetSearch");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("projetTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+  rechercheAppel() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("appelSearch");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("appelTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
 }
