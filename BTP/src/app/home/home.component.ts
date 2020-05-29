ef5e3ba69940d31755b7e3d830783c3ea3799ce4
Charline <charline>
2020-05-29 14:31:13 +0200

    home

--- BTP/src/app/home/home.component.ts
+++ BTP/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Projet} from '../model/projet';
 import {HomeService} from './home.service';
 import {Observable} from 'rxjs';
+import {Offre} from '../model/offre';
 
 @Component({
   selector: 'app-home',
@@ -11,20 +12,18 @@ import {Observable} from 'rxjs';
 export class HomeComponent implements OnInit {
 
   projets: Array<Projet> = new Array<Projet>();
+  offres: Array<Offre> = new Array<Offre>();
 
   constructor(private homeService: HomeService) {
-    this.list(7);
+    this.list(8);
   }
 
   ngOnInit(): void {
   }
 
-  // list(): Array<Projet>{
-  //   return  this.homeService.findAll();
-  // }
-
   list(id: number){
     this.homeService.findAllByMaitreOeuvre(id).subscribe(resp => this.projets = resp, error => console.log(error));
+    this.homeService.findAllByMaitreOeuvreEnConsult(id).subscribe(resp => this.offres = resp, error => console.log(error));
   }
 
 }
