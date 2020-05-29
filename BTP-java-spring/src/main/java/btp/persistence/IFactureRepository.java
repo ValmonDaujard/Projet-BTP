2c60017dbcd9118ec3f037a312b1ba1895a5ddf9
Brice <brice.lohrer@gmail.com>
2020-05-29 18:48:51 +0200

    Projet Angular MAJ

--- BTP-java-spring/src/main/java/btp/persistence/IFactureRepository.java
+++ BTP-java-spring/src/main/java/btp/persistence/IFactureRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import btp.model.Action;
 import btp.model.Facture;
 
 public interface IFactureRepository extends JpaRepository<Facture, Long>{
@@ -14,5 +15,8 @@ public interface IFactureRepository extends JpaRepository<Facture, Long>{
 	@Query("select f from Facture f where f.projet.id = :id and f.payee = :payee and f.prestataire.nom = :nomPresta")
 	List<Facture> findAllByProjetAndPayee(@Param("id") Long id, @Param("payee") Boolean payee, @Param("nomPresta") String nom);
 	
+	// Facture par projet
+	@Query("select f from Facture f where f.projet.id = :id")
+	List<Facture> findAllByProjet(@Param("id") Long id);
 	
 }
--- BTP/src/app/projet/projet.component.ts
+++ BTP/src/app/projet/projet.component.ts
@@ -7,6 +7,8 @@ import {Offre} from "../model/offre";
 import {Prestataire} from "../model/prestataire";
 import {Prestation} from "../model/prestation";
 import {Action} from "../model/action";
+import {Reunion} from "../model/reunion";
+import {Facture} from "../model/facture";
 
 @Component({
   selector: 'app-projet',
@@ -18,6 +20,9 @@ export class ProjetComponent implements OnInit {
   projet: Projet = null;
   prestationref: Array<Prestation> = new Array<Prestation>();
   actionref: Array<Action> = new Array<Action>();
+  reunionrefplanifie: Array<Reunion> = new Array<Reunion>();
+  reunionrefeffectue: Array<Reunion> = new Array<Reunion>();
+  factureref: Array<Facture> = new Array<Facture>();
 
 
   constructor(private projetService: ProjetService, private route: ActivatedRoute) {
@@ -31,6 +36,15 @@ export class ProjetComponent implements OnInit {
       }, error => console.log(error));
       this.projetService.findActions(parameters.id).subscribe(resp => {
         this.actionref = resp;
+      }, error => console.log(error));
+      this.projetService.findReunionsPlanifiees(parameters.id).subscribe(resp => {
+        this.reunionrefplanifie = resp;
+      }, error => console.log(error));
+      this.projetService.findReunionsEffectuees(parameters.id).subscribe(resp => {
+        this.reunionrefeffectue = resp;
+      }, error => console.log(error));
+      this.projetService.findFactures(parameters.id).subscribe(resp => {
+        this.factureref = resp;
       }, error => console.log(error));
     })
   }
