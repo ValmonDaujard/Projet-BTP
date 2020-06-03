31e2c30325659b9610bed550e452624edc319d3f
Brice <brice.lohrer@gmail.com>
2020-06-03 09:58:01 +0200

    Projet Angular Brice

--- BTP/src/app/model/facture.ts
+++ BTP/src/app/model/facture.ts
@@ -1,4 +1,7 @@
 import {Projet} from "./projet";
+import {Prestataire} from "./prestataire";
+import {MaitreOuvrage} from "./maitreOuvrage";
+import {MaitreOeuvre} from "./maitreOeuvre";
 
 export class Facture {
   id: number;
@@ -11,17 +14,17 @@ export class Facture {
   penaliteRetard: number;
   payee: boolean;
   quantite: number;
-  // prestataire: Prestataire;
-  // maitreOeuvre: MaitreOeuvre;
-  // maitreOuvrage: MaitreOuvrage;
+  prestataire: Prestataire;
+  maitreOeuvre: MaitreOeuvre;
+  maitreOuvrage: MaitreOuvrage;
   projet: Projet;
-  // prestation: Prestation;
-  // prestationSupplementaire: PrestationSupplementaire;
+/*  prestation: Prestation;
+  prestationSupplementaire: PrestationSupplementaire;*/
   factureMOEMO : boolean;
 
 
   constructor(id?: number, version?: number, numeroFacture?: number, dtFacturation?: Date, prixHT?: number,
-              remise?: number, dtEcheance?: Date, penaliteRetard?: number, payee?: boolean, projet?: Projet, factureMOEMO?: boolean) {
+              remise?: number, dtEcheance?: Date, penaliteRetard?: number, payee?: boolean, projet?: Projet, factureMOEMO?: boolean, prestataire?:Prestataire, maitreOeuvre?: MaitreOeuvre, maitreOuvrage?: MaitreOuvrage) {
     this.id = id;
     this.version = version;
     this.numeroFacture = numeroFacture;
@@ -33,5 +36,8 @@ export class Facture {
     this.payee = payee;
     this.projet = projet
     this.factureMOEMO=factureMOEMO;
+    this.maitreOeuvre=maitreOeuvre;
+    this.maitreOuvrage=maitreOuvrage;
+    this.prestataire=prestataire;
   }
 }
