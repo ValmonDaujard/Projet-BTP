568448624ecf16bd018b7e5f10b6dc3a6ad775d1
Charline <charline>
2020-06-03 14:09:03 +0200

    Auto stash before merge of "master" and "origin/master"

--- BTP/src/app/reponse-consultation/reponse-consultation.component.ts
+++ BTP/src/app/reponse-consultation/reponse-consultation.component.ts
@@ -12,19 +12,13 @@ import {Prestation} from '../model/prestation';
 })
 export class ReponseConsultationComponent implements OnInit {
 
-  infoPresta : Prestataire = new Prestataire();
-  infoMOE : MaitreOeuvre = new MaitreOeuvre();
   infoPrestation : Prestation = new Prestation();
 
   constructor(private reponseConsultationService: ReponseConsultationService, private route: ActivatedRoute) {
     this.route.params.subscribe(parameters => {
-      this.reponseConsultationService.findByIdPrestataire(646).subscribe(resp => {this.infoPresta = resp; console.log(resp);
-      }, error => console.log(error));
-      this.reponseConsultationService.findByIdMOE(637).subscribe(resp => {this.infoMOE = resp; console.log(resp);
-      }, error => console.log(error));
+      this.reponseConsultationService.findByIdPrestation(parameters.id).subscribe(resp => this.infoPrestation = resp, error => console.log(error));
     })
   }
-
   ngOnInit(): void {
   }
 
--- BTP/src/app/reponse-consultation/reponse-consultation.service.ts
+++ BTP/src/app/reponse-consultation/reponse-consultation.service.ts
@@ -10,20 +10,10 @@ import {Prestation} from '../model/prestation';
 })
 export class ReponseConsultationService {
 
-  private prestataires: Array<Prestataire> = new Array<Prestataire>();
-  private maitreOeuvres : Array<MaitreOeuvre> = new Array<MaitreOeuvre>();
   private prestations : Array<Prestation> = new Array<Prestation>();
 
   constructor(private http: HttpClient) {
     this.load();
-  }
-
-  findByIdPrestataire(id: number): Observable<Prestataire>{
-    return this.http.get<Prestataire>('http://localhost:8080/prestataire/' + id);
-  }
-
-  findByIdMOE(id: number): Observable<MaitreOeuvre>{
-    return this.http.get<MaitreOeuvre>('http://localhost:8080/maitreOeuvre/' + id);
   }
 
   findByIdPrestation(id:number): Observable<Prestation>{
@@ -31,12 +21,6 @@ export class ReponseConsultationService {
   }
 
   load(){
-    this.http.get<Array<Prestataire>>('http://localhost:8080/prestataire').subscribe(resp => {
-      this.prestataires = resp;
-    }, error => console.log(error));
-    this.http.get<Array<MaitreOeuvre>>('http://localhost:8080/maitreOeuvre').subscribe(resp => {
-      this.maitreOeuvres = resp;
-    }, error => console.log(error))
     this.http.get<Array<Prestation>>('http://localhost:8080/prestation').subscribe(resp => {
       this.prestations = resp;
     }, error => console.log(error));
